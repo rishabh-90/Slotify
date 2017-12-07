5515bbe24fdd928531992220cd65eb20449da624
rishabh-90 <rishabhaggarwal@outlook.com>
2017-12-06 20:21:58 -0700

    Data base

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
 	$timezone = date_default_timezone_set("Europe/London");
 
-	$con = mysqli_connect("localhost", "root", "", "slotify");
+	$con = mysqli_connect("rishabhaggarwal.000webhostapp.com", "id3911463_rishabh", "Balvikas123@", "id3911463_slotify");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
